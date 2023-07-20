52eccf1f3f40447e4b7873af74926fbeb551c932
Luisa Mora <79343096+LuisaMora@users.noreply.github.com>
2023-07-20 15:45:22 -0400

    Add files via upload

---
+++ facturador/src/test/java/com/examplefacturacion/facturador/FacturadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplefacturacion.facturador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
